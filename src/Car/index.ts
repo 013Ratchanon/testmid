504c25651e44aff809cf53a8af3a99c05380054b
Tae <136410460+013Ratchanon@users.noreply.github.com>
2025-03-02 21:54:13 +0700

    Update index.ts

--- src/Car/index.ts
+++ src/Car/index.ts
@@ -2,7 +2,7 @@ import { Car } from "./car";
 
 console.log(`################ Car ################`)
 const car = new Car('MG')
-car.accelerate(100)
-car.brake(20)
+car.accelerate(140)
+car.brake(30)
 console.log(car.toString())
 console.log(`################ Car ################`)
